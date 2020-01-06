deb0340364663fd3a62ac2de176e6dda7d10f75c
Jackson Kelvin de Souza <jkdsgg@gmail.com>
2020-01-06 00:10:37 -0400

    cloase dialog de component por emit de function

---
+++ src/components/Testes.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-content>
+        <v-container fluid>
+            <v-layout row wrap>
+                <v-card>
+                    <v-card-title>
+                        <span class="headline">Use Google's location service?</span>
+                    </v-card-title>
+                    <v-card-text>Lorem ipsum dolor sit amet, semper quis, sapien id natoque elit.</v-card-text>
+                    <v-card-actions>
+                        <v-btn color="error" text @click="closeDialog">Disagree</v-btn>
+                        <v-spacer></v-spacer>
+                        <v-btn color="success" text @click="null">Agree</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-layout>
+        </v-container>
+    </v-content>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            //
+        }),
+        computed:{
+            //
+        },
+        watch:{
+            //
+        },
+        methods: {
+            closeDialog: function() {
+                this.$emit('close-dialog')
+            }
+        }
+    };
+</script>
+
+<style scoped>
+</style>
--- src/views/Projeto.vue
+++ src/views/Projeto.vue
@@ -1,4 +1,45 @@
 <template>
+    <v-content>
+        <v-container>
+            <v-layout>
+                <v-flex>
+                    <v-btn color="primary" dark @click="dialog = true">Dialog</v-btn>
+
+                    <v-dialog v-model="dialog" width="350px" text>
+                        <Testes @close-dialog="closeDialog"></Testes>
+                    </v-dialog>
+                    <br>
+                    <p>valor do v-model do dialog do component: <strong>{{dialog}}</strong></p>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-content>
+</template>
+
+<script>
+    import Testes from "../components/Testes";
+    export default {
+        components: {Testes},
+        data: () => ({
+            dialog: false,
+        }),
+        computed:{
+            //
+        },
+        watch:{
+            //
+        },
+        methods: {
+            closeDialog(){
+                this.dialog = false
+            }
+        }
+    };
+</script>
+<style scoped>
+</style>
+<!--
+<template>
     <v-container class="white">
         <v-card>
             <vue-editor v-model="text" />
@@ -24,6 +65,6 @@
         }
     };
 </script>
-
 <style scoped>
 </style>
+-->
